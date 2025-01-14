ed702ad3b1eacb7bb04e738b00b0aae3d1b1e5b8
ShubhamGhumare11 <shubhamghumare@gtasterix.com>
2025-01-14 12:20:12 +0530

    chatbot changed

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/contact" element={<Contact />} />
 
        {/*   <Route path="/about" element={<About />} />
        <Route path="/numerology" element={<Numerology />} />
